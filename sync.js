ee6062461c880e3b312ec8aa8f2668549a82a3f1
Anton Iokov <it.iokov.gmail.com>
2024-04-06 16:35:01 +0200

    update message if it exists

--- sync.js
+++ sync.js
@@ -1,10 +1,11 @@
 import serverlessHttp from 'serverless-http';
 import { Telegraf } from 'telegraf';
 import express from 'express';
+import * as console from "node:console";
 
 
 const app = express();
-app.use(express.json()); // For parsing application/json request
+app.use(express.json());
 
 const token = process.env.BOT_TOKEN;
 if (token === undefined) {
@@ -13,16 +14,31 @@ if (token === undefined) {
 
 const bot = new Telegraf(token);
 
-// Define endpoint to receive external requests for bot to send message
-app.post('/new-game', (req, res) => {
-    const { chatId, message } = req.body;
+app.post('/sync-game', (req, res) => {
+    const { chatId, messageId, name, limit, participants, reserves } = req.body;
 
-    bot.telegram.sendMessage(chatId, message).then(() => {
-        res.sendStatus(200);
-    }).catch(err => {
-        res.status(500).send('Failed to send message');
-    });
+    const message = [
+        `* ${name}* \n*Limit*: ${limit} participants`,
+        `*Participants* \n${participants}`,
+        reserves ? `*Waitlist* \n${reserves}` : ''
+    ].join(`\n\n`);
+
+    if(!messageId) {
+        bot.telegram.sendMessage(chatId, message, { parse_mode: 'Markdown'} ).then((message) => {
+            console.log(message.message_id);
+            res.sendStatus(200);
+        }).catch(err => {
+            console.log(err);
+            res.status(500).send('Failed to send message');
+        });
+    } else {
+        bot.telegram.editMessageText(chatId, messageId, undefined, message).then(() => {
+            res.sendStatus(200);
+        }).catch(err => {
+            console.log(err);
+            res.status(500).send('Failed to edit message');
+        });
+    }
 });
 
-// Wrap your Express app instance with serverless-http, so that it can work with AWS Lambda
 export const handler = serverlessHttp(app);
