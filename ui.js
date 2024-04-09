9fc42c43c5e1c4b41eb91d95154ec8b42dee5007
Anton Iokov <it.iokov.gmail.com>
2024-04-09 17:34:51 +0200

    only allow to sign up once

--- ui.js
+++ ui.js
@@ -22,7 +22,7 @@ const fiberyApp = process.env.FIBERY_APP || 'Organizer';
 const getOrCreatePlayer = async (userId) => {
     const players = await fibery.entity.query({
         'q/from': `${fiberyApp}/Players`,
-        'q/select': ['fibery/id'],
+        'q/select': { id: 'fibery/id' },
         'q/where': ['=', [`${fiberyApp}/Telegram User ID`], '$user_id'],
         'q/order-by': [
             [['fibery/creation-date'], 'q/asc'],
@@ -41,7 +41,16 @@ const getOrCreatePlayer = async (userId) => {
 const getGame = async (chatId, messageId) => {
     const games = await fibery.entity.query({
         'q/from': `${fiberyApp}/Games`,
-        'q/select': ['fibery/id'],
+        'q/select': {
+            id: 'fibery/id',
+            signUps: {
+                'q/from': `${fiberyApp}/Sign-ups`,
+                'q/select': {
+                    player: [`${fiberyApp}/Player`, 'fibery/id']
+                },
+                'q/limit': 'q/no-limit'
+            }
+        },
         'q/where': ['and',
             ['=', [`${fiberyApp}/Telegram Chat ID`], '$chat_id'],
             ['=', [`${fiberyApp}/Telegram Message ID`], '$message_id']
@@ -68,14 +77,19 @@ bot.action('SIGN_UP', async (ctx) => {
         getOrCreatePlayer(ctx.callbackQuery.from.id),
         getGame(ctx.callbackQuery.message.chat.id, ctx.callbackQuery.message.message_id)
     ]);
-    // catch and inform user of the error if they can act on it
+    // TODO: catch and inform user of the error if they can act on it
+
+    const existingSignUps = game.signUps.filter(s => s.player === player.id);
+    if (existingSignUps.length > 0) {
+        return await ctx.answerCbQuery(`You are already signed up 🤷`);
+    }
 
     const currentDate = new Date();
     const signUp = await fibery.entity.createBatch([{
         'type': `${fiberyApp}/Sign-ups`,
         'entity': {
-            [`${fiberyApp}/Game`]: { 'fibery/id': game['fibery/id'] },
-            [`${fiberyApp}/Player`]: { 'fibery/id': player['fibery/id'] },
+            [`${fiberyApp}/Game`]: { 'fibery/id': game.id },
+            [`${fiberyApp}/Player`]: { 'fibery/id': player.id },
             [`${fiberyApp}/Signed up at`]: currentDate.toISOString(),
         }
     }]);
