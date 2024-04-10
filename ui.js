fb711b6a6987d1629d8af4cb261cd882bac83b7d
Anton Iokov <it.iokov.gmail.com>
2024-04-10 17:21:26 +0200

    improve logging and error handling in ui

--- ui.js
+++ ui.js
@@ -1,6 +1,7 @@
 import serverlessHttp from 'serverless-http';
 import { Telegraf } from 'telegraf';
 import Fibery from 'fibery-unofficial';
+import * as console from "node:console";
 
 const botToken = process.env.BOT_TOKEN;
 if (!botToken) {
@@ -20,6 +21,7 @@ const fibery = new Fibery({
 const fiberyApp = process.env.FIBERY_APP || 'Organizer';
 
 const getOrCreatePlayer = async (id, firstName, lastName, username) => {
+    console.log(`Looking for a Player by Telegram User ID: ${id.toString()}...`);
     const existingPlayers = await fibery.entity.query({
         'q/from': `${fiberyApp}/Player`,
         'q/select': { id: 'fibery/id' },
@@ -32,8 +34,10 @@ const getOrCreatePlayer = async (id, firstName, lastName, username) => {
     }, { '$user_id': id.toString() });
 
     if (existingPlayers.length === 1) {
+        console.log('Player found');
         return existingPlayers[0];
     } else {
+        console.log('Player not found, creating a new one...');
         const newPlayers = await fibery.entity.createBatch([{
             'type': `${fiberyApp}/Player`,
             'entity': {
@@ -44,11 +48,14 @@ const getOrCreatePlayer = async (id, firstName, lastName, username) => {
             }
         }]);
 
-        return { id: newPlayers[0]['fibery/id'] };
+        const newPlayerId = newPlayers[0]['fibery/id'];
+        console.log(`Player created: ${newPlayerId}`);
+        return { id: newPlayerId };
     }
 };
 
 const getGame = async (chatId, messageId) => {
+    console.log(`Looking for a Game by Telegram Chat ID (${chatId.toString()}) and Message ID (${messageId.toString()})...`);
     const games = await fibery.entity.query({
         'q/from': `${fiberyApp}/Game`,
         'q/select': {
@@ -89,26 +96,30 @@ bot.action('SIGN_UP', async (ctx) => {
     const user = ctx.callbackQuery.from;
     const message = ctx.callbackQuery.message;
 
-    const [player, game] = await Promise.all([
-        getOrCreatePlayer(user.id, user.first_name, user.last_name, user.username),
-        getGame(message.chat.id, message.message_id)
-    ]);
-    // TODO: catch and inform user of the error if they can act on it
+    try {
+        const [player, game] = await Promise.all([
+            getOrCreatePlayer(user.id, user.first_name, user.last_name, user.username),
+            getGame(message.chat.id, message.message_id)
+        ]);
 
-    const isRegistered = game.activeRegistrations.some(ar => ar.playerId === player.id);
-    if (isRegistered) {
-        return await ctx.answerCbQuery(`You are already signed up 🤷`);
-    }
-
-    const currentDate = new Date();
-    const newRegistration = await fibery.entity.createBatch([{
-        'type': `${fiberyApp}/Registration`,
-        'entity': {
-            [`${fiberyApp}/Game`]: { 'fibery/id': game.id },
-            [`${fiberyApp}/Player`]: { 'fibery/id': player.id },
-            [`${fiberyApp}/Signed up at`]: currentDate.toISOString(),
+        const isRegistered = game.activeRegistrations.some(ar => ar.playerId === player.id);
+        if (isRegistered) {
+            return await ctx.answerCbQuery(`You are already signed up 🤷`);
         }
-    }]);
+
+        const currentDate = new Date();
+        const newRegistration = await fibery.entity.createBatch([{
+            'type': `${fiberyApp}/Registration`,
+            'entity': {
+                [`${fiberyApp}/Game`]: { 'fibery/id': game.id },
+                [`${fiberyApp}/Player`]: { 'fibery/id': player.id },
+                [`${fiberyApp}/Signed up at`]: currentDate.toISOString(),
+            }
+        }]);
+    } catch (err) {
+        console.error(err);
+        return await ctx.answerCbQuery(`Something went wrong 😬`);
+    }
 
     return await ctx.answerCbQuery(`You've signed up 👌`);
 });
@@ -118,30 +129,34 @@ bot.action('OPT_OUT', async (ctx) => {
     const user = ctx.callbackQuery.from;
     const message = ctx.callbackQuery.message;
 
-    const [player, game] = await Promise.all([
-        getOrCreatePlayer(user.id, user.first_name, user.last_name, user.username),
-        getGame(message.chat.id, message.message_id)
-    ]);
-    // TODO: catch and inform user of the error if they can act on it
+    try {
+        const [player, game] = await Promise.all([
+            getOrCreatePlayer(user.id, user.first_name, user.last_name, user.username),
+            getGame(message.chat.id, message.message_id)
+        ]);
 
-    const activeRegistrations =  game.activeRegistrations.filter(ar => ar.playerId === player.id);
+        const activeRegistrations =  game.activeRegistrations.filter(ar => ar.playerId === player.id);
 
-    if (activeRegistrations.length === 0) {
-        return await ctx.answerCbQuery(`You don't have any active registrations 🤷`);
+        if (activeRegistrations.length === 0) {
+            return await ctx.answerCbQuery(`You don't have any active registrations 🤷`);
+        }
+
+        const currentDate = new Date();
+        const updates = activeRegistrations.map(ar => ({
+            'type': `${fiberyApp}/Registration`,
+            'entity': {
+                'fibery/id': ar.id,
+                [`${fiberyApp}/Opted out at`]: currentDate.toISOString()
+            }
+        }));
+
+        await fibery.entity.updateBatch(updates);
+    } catch (err) {
+        console.error(err);
+        return await ctx.answerCbQuery(`Something went wrong 😬`);
     }
 
-    const currentDate = new Date();
-    const updates = activeRegistrations.map(ar => ({
-        'type': `${fiberyApp}/Registration`,
-        'entity': {
-            'fibery/id': ar.id,
-            [`${fiberyApp}/Opted out at`]: currentDate.toISOString()
-        }
-    }));
-
-    const result = await fibery.entity.updateBatch(updates);
     return await ctx.answerCbQuery(`You've opted out 👌`);
 });
-
 
 export const handler = serverlessHttp(bot.webhookCallback("/bot"));
